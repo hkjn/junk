aebc4442bc051ce5203d43dda28aedb13d26dbea
Henrik Jonsson <me@hkjn.me>
2015-01-14 17:24:45 +0100

    specifies STAGE env variable=dev|prod, print it in junkapp

--- coreos/app/app.go
+++ coreos/app/app.go
@@ -5,10 +5,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Printf("app.go starting..\n")
+	fmt.Printf("app.go starting in stage %s..\n", os.Getenv("STAGE"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hi from app.go!\n")
 	})
