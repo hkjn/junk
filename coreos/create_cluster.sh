3d62c0f97221c9874485f6d502c90b0a9fe54ce9
Henrik Jonsson <me@hkjn.me>
2015-01-19 11:08:03 +0100

    3 machine cluster

--- coreos/create_cluster.sh
+++ coreos/create_cluster.sh
@@ -7,8 +7,7 @@ ZONE=europe-west1-b         # GCE zone
 MACHINE_TYPE=f1-micro       # GCE machine type
 # All valid images can be shown with:
 # gcloud compute images list --project coreos-cloud
-
-gcloud compute --project ${PROJECT_NAME} instances create core1 core2 \
+gcloud compute --project ${PROJECT_NAME} instances create core1 core2 core3 \
 		--image https://www.googleapis.com/compute/v1/projects/coreos-cloud/global/images/coreos-stable-522-4-0-v20150108 \
 		--zone ${ZONE} --machine-type ${MACHINE_TYPE} --metadata-from-file user-data=cloud-config.yaml
 
