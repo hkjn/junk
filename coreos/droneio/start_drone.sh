288c2716841d8ab10e49e67ad4e74697d8b4e243
Henrik Jonsson <me@hkjn.me>
2015-01-20 17:50:51 +0100

    fixes indentation

--- coreos/droneio/start_drone.sh
+++ coreos/droneio/start_drone.sh
@@ -4,7 +4,7 @@ docker run -d --name="drone-test" \
   --env DRONE_GITHUB_CLIENT=${DRONE_GITHUB_CLIENT} \
   --env DRONE_GITHUB_SECRET=${DRONE_GITHUB_SECRET} \
   -p 8080:8080 \
-	-v /var/lib/drone/ \
-	-v /var/run/docker.sock:/var/run/docker.sock \
-	-v /home/core/droneio/drone.sqlite:/var/lib/drone/drone.sqlite \
-	hkjn/drone:latest
+  -v /var/lib/drone/ \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /home/core/droneio/drone.sqlite:/var/lib/drone/drone.sqlite \
+  hkjn/drone:latest
