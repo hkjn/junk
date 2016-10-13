14f13e7de741a39aeaaeef28969f0287dedf4947
Henrik Jonsson <me@hkjn.me>
2016-10-13 06:44:13 +0200

    Update

--- terraform/aws_security_group.tf
+++ terraform/aws_security_group.tf
@@ -42,7 +42,7 @@ resource "aws_security_group" "allow_kube" {
 	# Allow all traffic from inside subnets.
   ingress {
     from_port = 0
-    to_port = 65535
+    to_port = 0
     protocol = "-1"
     cidr_blocks = "${split(",", var.public_ranges)}"
   }
