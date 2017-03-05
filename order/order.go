540a48187ef56d1758b76ea1be895a327cdc0656
Henrik Jonsson <me@hkjn.me>
2017-03-05 10:42:33 +0100

    WIP

--- order/order.go
+++ order/order.go
@@ -55,7 +55,7 @@ type (
 		ToCancel OrderNumber
 	}
 
-	// orderTree is the internal representation of Order items for the orderbook.
+	// orderTree is a representation of Order items for the orderbook.
 	//
 	// orderTree is a binary search tree that uses the price limit of orders as
 	// comparison key.
@@ -66,11 +66,14 @@ type (
 
 	// OrderBook holds all the orders.
 	OrderBook struct {
-		// orders is a BST for finding orders by price quickly.
-		orders *orderTree
-		//byNumber map[OrderNumber]*Order
-		nextOrder OrderNumber
-		cancelled map[OrderNumber]bool
+		// buyOrders holds the BuySide orders in the book.
+		buyOrders *orderTree
+		// sellOrders holds the SellSide orders in the book.
+		sellOrders *orderTree
+		// stopOrders holds the stop orders in the book which are not yet triggered.
+		stopOrders *orderTree
+		nextOrder  OrderNumber
+		cancelled  map[OrderNumber]bool
 	}
 
 	// Match is a match between two orders.
@@ -168,25 +171,25 @@ func (taker *Order) getMatch(maker *Order) *Match {
 		// A Cancel order can't match anything.
 		return nil
 	}
-	if maker.Type == Stop {
-		// A Stop order can't directly match anything as maker; it
-		// triggers when other trades execute when its threshold is
-		// reached. It can however be matched as taker.
-		return nil
-	}
+	//if maker.Type == Stop {
+	// A Stop order can't directly match anything as maker; it
+	// triggers when other trades execute when its threshold is
+	// reached. It can however be matched as taker.
+	//return nil
+	//}
 
 	match := false
 	price := Price(0.0)
 	// If the taker is a Stop or Market order, any value is acceptable
 	// for a match.
-	if taker.Type == Stop {
-		// Stop orders need to be triggered first.
-		if taker.stopTriggered {
-			match = true
-			// TODO: Correct? What if other side is Market too, with no Limit?
-			price = maker.Limit
-		}
-	} else if taker.Type == Market {
+	//if taker.Type == Stop {
+	// Stop orders need to be triggered first.
+	//if taker.stopTriggered {
+	//match = true
+	// TODO: Correct? What if other side is Market too, with no Limit?
+	//price = maker.Limit
+	//}
+	if taker.Type == Market {
 		match = true
 		price = maker.Limit
 	} else if taker.Side == BuySide && taker.Limit >= maker.Limit {
@@ -198,6 +201,7 @@ func (taker *Order) getMatch(maker *Order) *Match {
 	}
 
 	if !match {
+		log.Fatalf("No match; bug?\n")
 		return nil
 	}
 
@@ -205,7 +209,7 @@ func (taker *Order) getMatch(maker *Order) *Match {
 	if volume > maker.Volume {
 		volume = maker.Volume
 	}
-	debug("%q matches %q at %v, for %v units\n", taker, maker, price, volume)
+	debug("[MATCH] %q matches %q at %v, for %v units\n", taker, maker, price, volume)
 	return &Match{
 		Taker:  taker,
 		Maker:  maker,
@@ -296,20 +300,10 @@ func (order Order) String() string {
 	return "Order{???}"
 }
 
-func (order *Order) valid() bool {
-	// Skip cancelled, executed, !isTriggered
-	// TODO: Drop cancelled, we now hold hashset directly in orderbook
-	v := !order.cancelled && !order.executed
-	if order.Type == Stop {
-		v = v && order.stopTriggered
-	}
-	return v
-}
-
 // String returns a readable description of the Match.
 func (m Match) String() string {
 	return fmt.Sprintf(
-		"match between order %q and %q for %v units at $%v",
+		"match between order %v and %v for %v units at $%v",
 		m.Taker,
 		m.Maker,
 		m.Volume,
@@ -364,7 +358,7 @@ func debug(format string, a ...interface{}) {
 }
 
 func debugv(format string, a ...interface{}) {
-	if true {
+	if false {
 		fmt.Printf("[DD]     "+format, a...)
 	}
 }
@@ -420,75 +414,45 @@ func newOrderBook() OrderBook {
 	return OrderBook{
 		nextOrder: 1,
 		cancelled: map[OrderNumber]bool{},
-		//byNumber: map[OrderNumber]Order{},
 	}
-}
-
-// getMatches returns all Matches to specified Order.
-//
-// The results are given in descending priority, with the best match first.
-//
-// If multiple orders match the new order, price limit is the first
-// priority.
-//
-// If several matching orders have the same price limit, oldest order
-// is matched first.
-func (book *OrderBook) getMatches(taker *Order) Matches {
-	matches := Matches{}
-
-	// TODO: Handle missing Limit for Market
-	wantSide := SellSide
-	if taker.Side == SellSide {
-		wantSide = BuySide
-	}
-	for _, maker := range book.orders.fetch(taker.Limit, wantSide, taker.Type == Market) {
-		debug("looking to see if fetched maker %q is match\n", maker)
-		_, cancelled := book.cancelled[maker.id]
-		if cancelled {
-			continue
-		}
-		match := taker.getMatch(maker)
-		if match != nil {
-			matches = append(matches, match)
-		}
-	}
-	sort.Sort(sort.Reverse(matches))
-	return matches
 }
 
 // insert adds a new Order to the orders BST.
-func (t *orderTree) insert(order *Order) {
-	debug("insert(%q)\n", order)
-	c := t
-	for c != nil {
-		if order.Limit <= c.item.Limit {
-			debugv("inserting to left (lower)\n")
-			c.left = &orderTree{}
-			c = c.left
-		} else {
-			debugv("inserting to right (higher)\n")
-			c.right = &orderTree{}
-			c = c.right
-		}
-	}
-	// TODO: Handle missing Limit; c.key should be nil
-	// c.key = order.id
-	c.item = order
-}
+//func (t *orderTree) insert(order *Order) {
+//	debug("insert(%q)\n", order)
+//	c := t
+//	for c != nil {
+//		if order.Limit <= c.item.Limit {
+//			debugv("inserting to left (lower)\n")
+//			c.left = &orderTree{}
+//			c = c.left
+//		} else {
+//			debugv("inserting to right (higher)\n")
+//			c.right = &orderTree{}
+//			c = c.right
+//		}
+//	}
+//	// TODO: Handle missing Limit; c.key should be nil
+//	// c.key = order.id
+//	c.item = order
+//}
 
-func insert(root *orderTree, order *Order) *orderTree {
-	if root == nil {
-		root = &orderTree{
+// insert adds a new order to the orderTree and returns the new tree.
+//
+// TODO: Change from recursion to iteration, make method on orderTree.
+func insert(node *orderTree, order *Order) *orderTree {
+	if node == nil {
+		node = &orderTree{
 			item: order,
 		}
-	} else if order.Limit <= root.item.Limit {
+	} else if order.Limit <= node.item.Limit {
 		debugv("inserting %v to left (lower)\n", order.Limit)
-		root.left = insert(root.left, order)
+		node.left = insert(node.left, order)
 	} else {
 		debugv("inserting %v to right (higher)\n", order.Limit)
-		root.right = insert(root.right, order)
+		node.right = insert(node.right, order)
 	}
-	return root
+	return node
 }
 
 func traverse(t *orderTree, desc string) {
@@ -500,37 +464,58 @@ func traverse(t *orderTree, desc string) {
 	traverse(t.right, desc+"R")
 }
 
-// fetch traverses the BST and returns Order elements.
+// exec traverses the BST and returns the Match, if any.
 //
 // If max is true, orders with limit values >= specified value are returned.
 // If max is false, orders with limit values <= specified value are returned.
-func (orderTree *orderTree) fetch(limit Price, side OrderSide, isMarket bool) []*Order {
-	c := orderTree
-	results := []*Order{}
-	debug("fetch(%v, %v)\n", limit, side)
-	// TODO: We need to delete nodes that become executed or cancelled,
-	// or we can miss better matches.
+func (node *orderTree) exec(taker *Order, wantHighest bool) *Match {
+	c := node
+	makers := []*Order{}
+	debug("exec(%q)\n", taker)
+	// TODO: Handle missing Limit for Market
 	for c != nil {
-		if side == SellSide {
-			// Looking for orders with limit <= specified limit.
-			if c.item.Limit <= limit && c.item.Side == side && c.item.valid() {
-				results = append(results, c.item)
-				debugv("fetched order: %v\n", results)
-			}
-			debugv("fetching left (lower)\n")
-			c = c.left
-		} else {
-			// Looking for orders with limit >= specified limit.
-			if c.item.Limit >= limit && c.item.Side == side && c.item.valid() {
-				results = append(results, c.item)
-				debugv("fetched order: %v\n", results)
+		if wantHighest {
+			if c.item.Limit >= taker.Limit {
+				makers = append(makers, c.item)
+				debugv("fetched order: %v\n", makers)
 			}
 			debugv("fetching right (higher)\n")
 			c = c.right
+		} else {
+			if c.item.Limit <= taker.Limit {
+				makers = append(makers, c.item)
+				debugv("fetched order: %v\n", makers)
+			}
+			debugv("fetching left (lower)\n")
+			c = c.left
 		}
-		time.Sleep(time.Millisecond * 500)
+		time.Sleep(time.Millisecond * 200) // TODO: Remove
 	}
-	return results
+
+	matches := Matches{}
+	for _, maker := range makers {
+		matches = append(matches, taker.getMatch(maker))
+	}
+	sort.Sort(sort.Reverse(matches))
+
+	if len(matches) == 0 {
+		debug("exec() found no matches\n")
+		return nil
+	}
+	debugv("exec() got matches: %v\n", matches)
+
+	// Execute the order.
+	match := matches[0]
+	match.Maker.Volume -= match.Volume
+	if match.Maker.Volume <= 0.0 {
+		match.Maker.executed = true
+	}
+	match.Taker.Volume -= match.Volume
+	if match.Taker.Volume <= 0.0 {
+		match.Taker.executed = true
+	}
+	debug("exec() excuted %q\n", match)
+	return match
 }
 
 // delete removes an Order from the BST.
@@ -543,16 +528,35 @@ func (t *orderTree) delete(order *Order) {
 }
 
 // findStops returns all Stop orders triggered by specified price.
-func (t *orderTree) findStops(price Price) []*Order {
-	// TODO: Set stopTriggered = true for all stops triggered here:
-	// if stop.Side == BuySide && oldMatch.Price > stop.Value2 {
-	// The match triggers this BuySide Stop.
-	// stopp.stopTriggered = true
-	// } else if stop.Side == SellSide && oldMatch.Price < stop.Value2 {
-	// The match triggers this SellSide Stop.
-	// stopp.stopTriggered = true
-	// }
-	return nil
+func (book *OrderBook) findStops(order *Order) []*Order {
+	// TODO: Traverse stopOrders to find if order triggers any stop
+	// order. If it did, we should add that stop order to buyOrders or
+	// sellOrders, and execute that order if so.
+
+	c := book.stopOrders
+	wantHighest := order.Side == BuySide
+	triggered := []*Order{}
+	debug("findStops(%v)\n", order)
+	for c != nil {
+		if wantHighest {
+			if c.item.Limit >= order.Limit {
+				triggered = append(triggered, c.item)
+				debugv("fetched order: %v\n", triggered)
+			}
+			debugv("fetching right (higher)\n")
+			c = c.right
+		} else {
+			if c.item.Limit <= order.Limit {
+				triggered = append(triggered, c.item)
+				debugv("fetched order: %v\n", triggered)
+			}
+			debugv("fetching left (lower)\n")
+			c = c.left
+		}
+		time.Sleep(time.Millisecond * 200) // TODO: Remove
+	}
+
+	return triggered
 }
 
 // Add adds and attempts to execute an Order.
@@ -562,62 +566,65 @@ func (t *orderTree) findStops(price Price) []*Order {
 //
 // If there's matching orders, they are executed, and the resulting
 // matches are returned.
-func (book *OrderBook) Add(order *Order) Matches {
-	//book.byNumber[order.id] = order
-	if order.Type == Cancel {
-		book.cancelled[order.ToCancel] = true
-		// toCancel := book.orders.lookup(order.ToCancel)
-		// If order to cancel didn't exist or was fully executed, it
-		// can't be cancelled. Weird, but this is supposed to be no-op.
-		//if toCancel != nil && !toCancel.executed {
-		//toCancel.cancelled = true
-		debug("Cancelled %v\n", order.ToCancel)
-		//}
-		book.nextOrder++
-		return nil
-	}
-
-	order.id = book.nextOrder
-	book.orders = insert(book.orders, order)
-	// debug("book.orders=%+v\n", book.orders)
-	traverse(book.orders, ".")
+func (book *OrderBook) Add(taker *Order) Matches {
+	taker.id = book.nextOrder
 	book.nextOrder++
-	info("Added order %q\n", order)
-
-	// Stop orders don't match as taker, but they can be triggered
-	// after other orders execute.
-	if order.Type == Stop {
+	if taker.Type == Cancel {
+		book.cancelled[taker.ToCancel] = true
+		// TODO: Delete taker.ToCancel from appropriate tree here by
+		// traversing them and then deleting the order.
+		debug("Cancelled %v\n", taker.ToCancel)
+		return nil
+	}
+	if taker.Type == Stop {
+		debug("Added stop order %v\n", taker)
+		book.stopOrders = insert(book.stopOrders, taker)
 		return nil
 	}
 
+	if taker.Side == BuySide {
+		book.buyOrders = insert(book.buyOrders, taker)
+	} else {
+		book.sellOrders = insert(book.sellOrders, taker)
+	}
+	info("Added order %q\n", taker)
+
+	// TODO: Need to pass on a reference to the book.buyOrders *orderTree here to exec, so it can
+
+	// Look for matches for the recently added order.
 	matches := Matches{}
-	for !order.executed {
-		newMatches := book.getMatches(order)
-		if len(newMatches) == 0 {
-			debug("No matches\n")
+	for !taker.executed {
+		var match *Match
+		if taker.Side == BuySide {
+			match = book.sellOrders.exec(taker, false)
+		} else {
+			match = book.buyOrders.exec(taker, true)
+		}
+		if match == nil {
+			debug("No new matches, returning the ones we have: %v\n", matches)
 			return matches
 		}
-		debug("Got new matches: %v\n", newMatches)
-		order = book.execute(newMatches[0])
-		// TODO: Both taker and maker are executed here, need to be deleted from BST.
-		book.orders.delete(newMatches[0].Taker)
-		book.orders.delete(newMatches[0].Maker)
-		matches = append(matches, newMatches[0])
+		debug("Adding best new match found: %v\n", match)
+		matches = append(matches, match)
+		// TODO: Both taker and maker are executed if we made it here, need
+		// to be deleted from BST.
+		if taker.Side == BuySide {
+			if match.Taker.executed {
+				book.buyOrders.delete(match.Taker)
+			}
+			if match.Maker.executed {
+				book.sellOrders.delete(match.Maker)
+			}
+		} else {
+			if match.Taker.executed {
+				book.sellOrders.delete(match.Taker)
+			}
+			if match.Maker.executed {
+				book.buyOrders.delete(match.Maker)
+			}
+		}
 	}
 	return matches
-}
-
-func (book *OrderBook) execute(match *Match) *Order {
-	info("Executing: %v\n", match)
-	match.Maker.Volume -= match.Volume
-	if match.Maker.Volume <= 0.0 {
-		match.Maker.executed = true
-	}
-	match.Taker.Volume -= match.Volume
-	if match.Taker.Volume <= 0.0 {
-		match.Taker.executed = true
-	}
-	return match.Taker
 }
 
 // getStopMatches returns any matches for stop orders triggered by the matches.
@@ -629,15 +636,35 @@ func (book *OrderBook) getTriggeredStops(oldMatches Matches) Matches {
 		// this price is below any SellSide Stop order's Limit, or above
 		// any BuySide Stop order's Limit. If so, they trigger and we want
 		// to match + execute them, and return the matches.
-		for _, stop := range book.orders.findStops(oldMatch.Price) {
-			for !stop.executed {
-				newMatches := book.getMatches(stop)
-				if len(newMatches) == 0 {
-					break
+
+		for _, taker := range book.findStops(oldMatch.Taker) {
+			// Any Order in triggered is a Stop order which was triggered by
+			// recent executions. They should be removed from stopOrders, and
+			// added to buyOrders / sellOrders.
+			debug("Should delete stopOrder %v\n", taker)
+			book.stopOrders.delete(taker)
+			if taker.Side == BuySide {
+				debug("Adding triggered stoporder to buyOrders: %q\n", taker)
+				book.buyOrders = insert(book.buyOrders, taker)
+			} else {
+				debug("Adding triggered stoporder to sellOrders: %q\n", taker)
+				book.sellOrders = insert(book.sellOrders, taker)
+			}
+
+			// Look for matches for the recently triggered stop order.
+			for !taker.executed {
+				var match *Match
+				if taker.Side == BuySide {
+					match = book.sellOrders.exec(taker, false)
+				} else {
+					match = book.buyOrders.exec(taker, true)
 				}
-				book.execute(newMatches[0])
-				debug("stop order was executed: %v\n", stop)
-				matches = append(matches, newMatches[0])
+				if match == nil {
+					debug("No new matches, returning the ones we have: %v\n", matches)
+					return matches
+				}
+				debug("Adding best new match found: %v\n", match)
+				matches = append(matches, match)
 			}
 		}
 	}
@@ -654,7 +681,7 @@ func main() {
 		}
 		order := newOrder(orderstr)
 		// TODO: Maybe type returned here should be Executions or
-		// something; "matches" is misleading..
+		// something; "matches" is misleading since we already executed them.
 		matches := book.Add(order)
 		for _, match := range matches {
 			fmt.Println(match.Output())
